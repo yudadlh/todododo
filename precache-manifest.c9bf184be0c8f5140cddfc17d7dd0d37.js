25a9cc15e86f74e05efb1f56997e8318b0a29b5e
yudadlh <yudhalh21@gmail.com>
2022-06-25 02:48:02 +0700

    Updates

---
+++ precache-manifest.c9bf184be0c8f5140cddfc17d7dd0d37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1226123368a7eb014b0b",
+    "url": "/todododo/static/css/main.dbb3760b.chunk.css"
+  },
+  {
+    "revision": "1226123368a7eb014b0b",
+    "url": "/todododo/static/js/main.12261233.chunk.js"
+  },
+  {
+    "revision": "9f64384e27aacfe2e172",
+    "url": "/todododo/static/js/1.9f64384e.chunk.js"
+  },
+  {
+    "revision": "de24771859dcd398afbf",
+    "url": "/todododo/static/js/runtime~main.de247718.js"
+  },
+  {
+    "revision": "2d504b7cceb28c02cba53a2029f94ef4",
+    "url": "/todododo/index.html"
+  }
+];
